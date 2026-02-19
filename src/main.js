0d9e7fa5a430f8aa9a661a41864c37a613ae0abb
abaysankar <abaysankar777@gmail.com>
2026-02-19 20:25:55 +0530

    added basic docker file and docker ignore

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+const path = process.cwd()
+config({ path: `${path}/.env` });
+
+
+const app = express();
+const port = process.env.PORT
+
+app.get('/', (req,res) => {
+  return res.json({ data: "welcome to the world" })
+})
+
+app.listen(port, () => {
+  console.log("succesfully running at port ", port)
+})
